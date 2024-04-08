d7b68f2f7f19b93658322c487d1740bcb23e175b
Valerii Serediouk <waler4ik28@gmail.com>
2024-04-08 21:45:39 +0200

    Refactor provider concept, now the provider are wired automatically

--- internal/tmpl/tmpl.go
+++ internal/tmpl/tmpl.go
@@ -1,18 +1,25 @@
 package tmpl
 
 type Package struct {
-	Name     string
-	RelPath  string
-	NameCaps string
+	Name    string
+	RelPath string
 }
 
 type WSPackage struct {
 	Package
 	RoutePath string
+	NameCaps  string
+}
+
+type StructInfo struct {
+	Package
+	StructName string
 }
 
 type Root struct {
-	ModulePath string
-	Packages   []Package
-	WSPackages []WSPackage
+	ModulePath      string
+	Packages        []Package
+	WSPackages      []WSPackage
+	ProviderStructs []StructInfo
+	SecretManagers  []StructInfo
 }
