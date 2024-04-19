750941067432d30e7c0322049d68338aaa7080db
Valerii Serediouk <waler4ik28@gmail.com>
2024-04-19 15:11:35 +0200

    Add initial test structure

--- main_test.go
+++ main_test.go
@@ -1,28 +1,51 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"io"
+	"os/exec"
 	"testing"
+
+	"github.com/cucumber/godog"
 )
 
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-		args []string
-	}{
-		{
-			name: "init",
-			args: []string{"kk", "init", "--modulepath=github.com/yourworkspace/CoolModuleName"},
-		},
-		{
-			name: "add",
-			args: []string{"kk", "add", "resource", "customer"},
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t, // Testing instance that will run subtests.
 		},
 	}
-	for _, tt := range tests {
-		os.Args = tt.args
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
 	}
+}
+
+func InitializeScenario(sc *godog.ScenarioContext) {
+	//sc.Step(`^there are (\d+) godogs$`, thereAreGodogs)
+	//sc.Step(`^I eat (\d+)$`, iEat)
+	//sc.Step(`^there should be (\d+) remaining$`, thereShouldBeRemaining)
+}
+
+func runCmdWithArgs(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("stderrpipe: %w", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("start: %w", err)
+	}
+
+	slurp, _ := io.ReadAll(stderr)
+	fmt.Printf("%s\n", slurp)
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("wait: %w", err)
+	}
+	return nil
 }
